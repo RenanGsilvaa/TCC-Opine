2b2c4f0874e453542ebdc0347e7b64ebcca143c0
RenanGsilvaa <profissionalrenann@outlook.com>
2023-03-06 20:41:02 -0300

    Arrumado o porblema do ForEach(Obrigado Luis)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.main')
 
 @section('content')
-@forech($users as $user)
-    <h1>{{$user -> name}}</h1>
-    <h2>{{$user -> cpf}}</h2>
-@endforech
+@foreach($users as $user)
+    <h1>{{$user-> name}}</h1>
+    <h2>{{$user-> cpf}}</h2>
+@endforeach
 @endsection
