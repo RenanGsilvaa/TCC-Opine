eb781a0303fee7a08a978705f0ccf2bc30a1137d
RenanGsilvaa <profissionalrenann@outlook.com>
2023-03-07 22:20:03 -0300

    Fazendo aparecer os nomes e cpf dos usuraios

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 @foreach($users as $user)
-    <h1>{{$user-> name}}</h1>
-    <h2>{{$user-> cpf}}</h2>
+    <h1>{{$user -> nome}}</h1>
+    <h2>{{$user -> cpf}}</h2>
 @endforeach
 @endsection
